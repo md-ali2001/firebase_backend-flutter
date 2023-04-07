be651be6b358c9bfc779eb243d5b8b1a688f1ebd
M.A <md.ali2001@hotmail.com>
2023-04-07 13:47:13 -0700

    configured flutter with firebase (multi-platform)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+// ...
 
   // This widget is the root of your application.
   @override
